bc055430cb36e7f6dd80574597b0d79d2ce70c77
torsinclair <tor@romanesco.se>
2016-03-01 10:00:15 -0800

    Complete _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -79,6 +79,13 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+    _.each(collection, function(value){
+      if(test(value)){
+        results.push(value);
+      }
+    });
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
