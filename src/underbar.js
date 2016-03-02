ac7b28dc5c77d8c7eba2b69f8ac2144322e876c3
torsinclair <tor@romanesco.se>
2016-03-01 20:03:32 -0800

    Incomplete _.sortBy

--- src/underbar.js
+++ src/underbar.js
@@ -379,6 +379,7 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    
   };
 
   // Zip together two or more arrays with elements of the same index
